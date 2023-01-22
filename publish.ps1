e2d3d38b02779cfe20431d471423b09cbce711b0
Myrmex <dfusi@hotmail.com>
2023-01-22 15:39:29 +0100

    Update publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -4,6 +4,6 @@ dotnet publish -c Release -r win-x64
 dotnet publish -c Release -r linux-x64
 dotnet publish -c Release -r osx-x64
 
-compress-archive -path .\blob\bin\Release\net6.0\win-x64\publish\* -DestinationPath .\blob\bin\Release\net6.0\win-x64\blob-win-x64.zip -Force
-compress-archive -path .\blob\bin\Release\net6.0\linux-x64\publish\* -DestinationPath .\blob\bin\Release\net6.0\linux-x64\blob-linux-x64.zip -Force
-compress-archive -path .\blob\bin\Release\net6.0\osx-x64\publish\* -DestinationPath .\blob\bin\Release\net6.0\osx-x64\blob-osx-x64.zip -Force
+compress-archive -path .\blob\bin\Release\net7.0\win-x64\publish\* -DestinationPath .\blob\bin\Release\blob-win-x64.zip -Force
+compress-archive -path .\blob\bin\Release\net7.0\linux-x64\publish\* -DestinationPath .\blob\bin\Release\blob-linux-x64.zip -Force
+compress-archive -path .\blob\bin\Release\net7.0\osx-x64\publish\* -DestinationPath .\blob\bin\Release\blob-osx-x64.zip -Force
